1ea54159dd59481a85a321d0e2fb3fbaa50bc674
Vonster1 <hvons1600@alumno.ipn.mx>
2025-05-07 17:52:47 -0600

    modificaciones

--- P15_Torch_Convolucion.py
+++ P15_Torch_Convolucion.py
@@ -59,7 +59,79 @@ imshow(torchvision.utils.make_gird(images))
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConNet, self).__init__()
-        self.conv1 = nn
+        self.conv1 = nn.Conv2d(3,6,5)
         self.pool = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(6,16,5)
+        self.fc1 = nn.Linear(16*5*5,120)
+        self.fc2 = nn.Linear(120,84)
+        self.fc3 = nn.Linear(84,10)
+
+    def forward(self,x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1,16*5,5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# CORRER EL MODELO EN EL GPU
+model = ConvNet().to(device)
+
+# USAR CROSS-ENTROPY COMO COSTO Y GRADIENTE ESTOCÁSTICO COMO OPTIMIZADOR
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+
+# ITERACIONES (ENTRENAMIENTO)
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images,labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        optimizer.zero_grad()
+        loss.backeward()
+        optimizer.step()
+
+        if(i+1) % 200 == 0:
+            print(f'Epoch [{epoch+1}/{num_epoch}], Step[{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
+
+print('Entrenaimiento completo')
+
+# GUARDAR RESULTADO DEL MODELO (PARAMETROS)
+PATH = './cnn.pth'
+torch.save(model.state_dict(), PATH)
+
+# PROBAR EL MODELO
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    n_class_correct = [0 for i in range(10)]
+    n_class_samples = [0 for i in range(10)]
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+
+        for i in range(batch_size):
+            label = labels[i]
+            pred = predicted[i]
+            if(label == pred):
+                n_class_correct[label] += 1
+            n_class_samples[label] += 1
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Precision del modelo: {acc} %')
+
+    for i in range(10):
+        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+        print(f'Precision de {classes[i]}: {acc}%')
+
+
+
 
 
